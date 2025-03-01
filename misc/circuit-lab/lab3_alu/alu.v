ee71efdee3e1ac7370db990b208077fbe6384e41
zhuguiyuan <951620305@qq.com>
2025-03-01 16:32:21 +0800

    Circuit-Lab: Improve the performance

--- misc/circuit-lab/lab3_alu/alu.v
+++ misc/circuit-lab/lab3_alu/alu.v
@@ -8,30 +8,52 @@ module alu (
     output wire       zf
 );
 
-    //         sub          || compare      || equal
-    wire cin = op == 3'b001 || op == 3'b110 || op == 3'b111;
-    wire [3:0] b_inner, alu_out;
-    reg  [3:0] r_inner;
+    // pre-compute add and sub
+    wire [3:0] not_b = ~b;
+    wire [3:0] r_add, r_sub;
+    wire       cf_add, cf_sub;
 
-    assign b_inner = {4{cin}} ^ b;
-    assign {cf, alu_out} = a + b_inner + {3'b0, cin};
+    assign {cf_add, r_add} = a + b;
+    assign {cf_sub, r_sub} = a + not_b + 1;
+
+    // for always block
+    reg [3:0] r_inner;
+    reg       cf_inner, of_inner, zf_inner;
 
     always @(*) begin
+        {cf_inner, r_inner} = 0;
         case (op)
-            3'b000: r_inner = alu_out;
-            3'b001: r_inner = alu_out;
+            3'b000: {cf_inner, r_inner} = {cf_add, r_add};
+            3'b001: {cf_inner, r_inner} = {cf_sub, r_sub};
             3'b010: r_inner = ~a;
             3'b011: r_inner = a & b;
             3'b100: r_inner = a | b;
             3'b101: r_inner = a ^ b;
             3'b110: r_inner = {3'b0, $signed(a) < $signed(b)};
-            3'b111: r_inner = {3'b0, alu_out == 0};
-            default r_inner = 0;
+            3'b111: r_inner = {3'b0, r_sub == 0};
+        endcase
+    end
+
+    // bypass zf_inner to get better performance
+    always @(*) begin
+        case (op)
+            3'b000: zf_inner = r_add == 0;
+            3'b001: zf_inner = r_sub == 0;
+            default zf_inner = r_inner == 0;
+        endcase
+    end
+
+    always @(*) begin
+        case (op)
+            3'b000: of_inner = a[3] == b[3] && a[3] != r_add[3];
+            3'b001: of_inner = a[3] != b[3] && a[3] != r_sub[3];
+            default: of_inner = 0;
         endcase
     end
 
     assign r = r_inner;
-    assign of = (a[3] == b_inner[3]) && (a[3] != alu_out[3]);
-    assign zf = r == 0;
+    assign cf = cf_inner;
+    assign of = of_inner;
+    assign zf = zf_inner;
 
 endmodule
