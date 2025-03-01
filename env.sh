eaf681636c9b532d74c2573f4aabf13fa200d059
zhuguiyuan <951620305@qq.com>
2025-03-01 22:30:19 +0800

    Add script for migration

---
+++ env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# usage: addenv env_name path
+function addenv() {
+  echo "export $1=$(readlink -f $2)"
+  export $1=$(readlink -f $2)
+}
+
+# usage: init repo branch directory
+function init() {
+  if [ -d $3 ]; then
+    echo "$3 is already initialized, skipping..."
+    return
+  fi
+
+  while [ ! -d $3 ]; do
+    git clone -b $2 git@github.com:$1.git $3
+  done
+}
+
+init NJU-ProjectN/fceux-am ics2021 fceux-am
+init NJU-ProjectN/nvboard master nvboard
+
+addenv NEMU_HOME nemu
+addenv AM_HOME abstract-machine
+addenv NPC_HOME npc
+addenv NVBOARD_HOME nvboard
+addenv STA_HOME yosys-sta
