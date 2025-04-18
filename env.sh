6df32acf30e7f9784629c65ec0fc016c3b8ce353
zhuguiyuan <951620305@qq.com>
2025-04-18 19:03:44 +0800

    Improve env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    echo "This script is designed to be sourced, not executed directly."
+    exit 1
+fi
+
 # usage: addenv env_name path
 function addenv() {
   echo "export $1=$(readlink -f $2)"
@@ -18,17 +23,28 @@ function init() {
   done
 }
 
+function warn() {
+  echo -e "\e[33m$*\e[0m"
+}
+
+warn "Install essential packages"
 sudo apt install build-essential man gcc-doc gdb git \
   libreadline-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev \
   libfmt-dev
 
+warn "Ensure submodules exist"
 init NJU-ProjectN/fceux-am ics2021 fceux-am
 init NJU-ProjectN/nvboard master nvboard
 
+warn "Now you should add following lines to .bashrc"
 addenv NEMU_HOME nemu
 addenv AM_HOME abstract-machine
 addenv NPC_HOME npc
 addenv NVBOARD_HOME nvboard
 addenv STA_HOME yosys-sta
 
+warn "Initialize Yosys-STA"
 pushd "$STA_HOME" && make init && popd
+
+warn "Finally you should goto https://github.com/YosysHQ/oss-cad-suite-build to download Yosys, Icarus Verilog, and GtkWave"
+
