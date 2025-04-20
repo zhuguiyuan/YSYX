c8e6e70afbcacbfa79de7347de40df63eaa4e4a7
zhuguiyuan <951620305@qq.com>
2025-04-20 15:19:52 +0800

    Update env.sh with flex and bison

--- env.sh
+++ env.sh
@@ -30,7 +30,7 @@ function warn() {
 warn "Install essential packages"
 sudo apt install build-essential man gcc-doc gdb valgrind git wget \
   libreadline-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev \
-  libfmt-dev libgoogle-glog-dev xxd llvm-dev
+  libfmt-dev libgoogle-glog-dev xxd llvm-dev bash-completion flex bison
 
 warn "Ensure submodules exist"
 init NJU-ProjectN/fceux-am ics2021 fceux-am
