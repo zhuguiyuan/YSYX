54d2d0d1e7f8ace622a7078213f9e6ddc3b28b81
zhuguiyuan <951620305@qq.com>
2026-09-25 20:28:51 +0800

    feat: 通过 bundle_fifo 的仿真梳理时序模型

--- misc/verilator-demo/bundle-fifo/bundle_fifo_sim.cc
+++ misc/verilator-demo/bundle-fifo/bundle_fifo_sim.cc
@@ -1,57 +1,61 @@
 #include <Vtop.h>
 #include <cassert>
 #include <cstdio>
-#include <memory>
 #include <verilated_fst_c.h>
 
-constexpr auto SIM_ITERS = 1000;
+constexpr auto SIM_ITERS = 30;
 
-int main(void) {
-  auto contextp = std::make_unique<VerilatedContext>();
-  // Top don't take the ownership of contextp raw ptr
-  auto top = std::make_unique<Vtop>(contextp.get());
+struct SimulationState {
+  VerilatedContext ctx;
+  Vtop top;
+  VerilatedFstC tfp;
 
-  // Enable the fst waveform
-  Verilated::traceEverOn(true);
-  auto tfp = std::make_unique<VerilatedFstC>();
-  top->trace(tfp.get(), 99);
-  tfp->open("wave.fst");
-
-  // reset the model
-  auto reset = [&]() {
-    top->rst = 0;
-    top->clk = 1;
-    top->eval();
-    top->enq_valid = 0;
-    top->enq_data = 0;
-    top->deq_ready = 0;
-    tfp->dump(contextp->time());
-  };
-
-  auto cycle = [&](std::function<void()> driver) {
-    // tick
-    contextp->timeInc(1);
-    top->clk = 0;
-    top->eval();
-    tfp->dump(contextp->time());
-    // tock
-    contextp->timeInc(1);
-    top->clk = 1;
-    top->eval();
-    driver();
-    tfp->dump(contextp->time());
-  };
-
-  auto driver = [&]() {
-    top->enq_data = rand() & 1;
-    top->enq_valid = rand() & 1;
-    top->deq_ready = rand() & 1;
-  };
-
-  reset();
-  for (int i = 0; i < SIM_ITERS; ++i) {
-    cycle(driver);
+  SimulationState() : ctx{}, top{&ctx}, tfp{} {
+    // Enable the fst waveform
+    Verilated::traceEverOn(true);
+    top.trace(&tfp, 99);
+    tfp.open("wave.fst");
   }
 
+  void cycle() {
+    tfp.dump(ctx.time());
+    ctx.timeInc(1);
+    top.clk = 0;
+    top.eval();
+    tfp.dump(ctx.time());
+    ctx.timeInc(1);
+    top.clk = 1;
+    top.eval();
+  }
+
+  void randomDrive() {
+    top.enq_data = rand() & 1;
+    top.enq_valid = rand() & 1;
+    top.deq_ready = rand() & 1;
+  };
+
+  void reset() {
+    top.rst = 1;
+    top.enq_valid = 0;
+    top.enq_data = 0;
+    top.deq_ready = 0;
+    for (int i = 0; i < 10; ++i) {
+      cycle();
+    }
+    top.rst = 0;
+  }
+};
+
+int main(void) {
+  SimulationState state{};
+  state.reset();
+  for (int i = 0; i < SIM_ITERS; ++i) {
+    printf("%ld %d %d %d\t", state.ctx.time(), state.top.enq_data,
+           state.top.enq_valid, state.top.enq_ready);
+    printf("%ld %d %d %d\n", state.ctx.time(), state.top.deq_data,
+           state.top.deq_valid, state.top.deq_ready);
+    state.randomDrive();
+    state.cycle();
+  }
   return 0;
 }
